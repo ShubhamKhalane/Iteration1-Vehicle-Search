bf60a0c72fbf61652b29d53dd83520ab3ec1d7cc
ShubhamKhalane <snkhalanesk@gmail.com>
2023-06-06 17:09:29 +0530

    Adding Getall Method

--- src/main/java/com/exampleiteration1/org/controller/ManufacturerController.java
+++ src/main/java/com/exampleiteration1/org/controller/ManufacturerController.java
@@ -6,9 +6,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("api/v1/manufacturer")
@@ -19,5 +22,11 @@ public class ManufacturerController {
     ResponseEntity<Manufacturer> saveManufacturer(@RequestBody Manufacturer manufacturer){
         Manufacturer dbsaveman = manufacturerService.saveManufactorer(manufacturer);
         return new ResponseEntity<>(dbsaveman, HttpStatus.OK);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Manufacturer>> getAllManufacturer(){
+        List<Manufacturer> man = manufacturerService.fetchAll();
+        return ResponseEntity.status(HttpStatus.OK).body(man);
     }
 }
