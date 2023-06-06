74229b3a901686da7e05f8eb2e059995d5cd1de8
ShubhamKhalane <snkhalanesk@gmail.com>
2023-06-06 17:59:08 +0530

    DS2 - Resolving Defect in GetById Method

---
+++ src/main/java/com/exampleiteration1/org/exception/ManufacturerNotFoundException.java
@@ -0,0 +1,7 @@
+package com.exampleiteration1.org.exception;
+
+public class ManufacturerNotFoundException extends Exception{
+    public ManufacturerNotFoundException(String message){
+        super(message);
+    }
+}
